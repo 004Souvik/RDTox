bb29301842cdeb15b36a30400c5c8aa07e23a0e5
004Souvik <123533712+004Souvik@users.noreply.github.com>
2026-01-12 00:03:28 +0530

    Update RDTox.py

--- RDTox.py
+++ RDTox.py
@@ -41,6 +41,9 @@ show_preview = st.sidebar.checkbox("Show uploaded dataset preview", value=True)
 run_button = st.sidebar.button("Run prediction")
 
 st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
+st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
+st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
+st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
 
 try:
     with open("Sample.xlsx", "rb") as f:
@@ -131,6 +134,7 @@ else:
 
 # small footer
 st.markdown("---")
+
 
 
 
